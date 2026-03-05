d928d8d635e91d851700c9a13dd514ca3200c9f0
n.kamalov <n.kamalov@uic.group>
2026-03-05 08:40:36 +0500

    refactor: simplify leftRightDifference function and add leftRightDifference42 implementation

--- problems/arrays/leftRightDifference.py
+++ problems/arrays/leftRightDifference.py
@@ -3,9 +3,8 @@ def leftRightDifference(nums: list) -> list:
     left_sum = 0
     result = []
 
-    for i in range(len(nums)):
-        master_force = nums[i]
-        right_sum = total_sum - left_sum - master_force
+    for num in nums:
+        right_sum = total_sum - left_sum - num
 
         if left_sum > right_sum:
             result.append(-1)
@@ -14,13 +13,34 @@ def leftRightDifference(nums: list) -> list:
         else:
             result.append(0)
 
-        left_sum += master_force
+        left_sum += num
     
+    return result
+
+
+def sign(diff):
+    if diff > 0:
+        return 1
+    elif diff < 0:
+        return -1
+
+    return 0
+
+
+def leftRightDifference42(nums: list) -> list:
+    left, right = 0, sum(nums)
+    result = []
+
+    for num in nums:
+        right -= num
+        result.append(sign(right - left))
+        left += num
+
     return result
 
 
 if __name__ == "__main__":
     nums = [10, 4, 8, 3]
-    result = leftRightDifference(nums)
+    result = leftRightDifference42(nums)
 
-    print(result)
+    print(result)   
